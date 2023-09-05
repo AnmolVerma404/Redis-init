1969b2e6e46422e622938fce8d1f8197bfd23679
Anmol Verma <anmolverma102002@gmail.com>
2023-09-05 15:52:43 +0530

    fix: :bug: fixed redis instance closed bug

--- server.js
+++ server.js
@@ -1,19 +1,40 @@
 const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
+const Redis = require('redis');
+
+const DEFAULT_EXPIRATION = 3600;
+
+/**
+ * @param empty for local environment
+ * @param '{url}' give the url of your redis instance
+ */
+const redisClient = Redis.createClient();
 
 const app = express();
 app.use(cors());
 
 app.get('/photos', async (req, res) => {
 	const albumId = req.query.albumId;
-	const { data } = await axios.get(
-		'http://jsonplaceholder.typicode.com/photos',
-		{
-			params: { albumId },
-		}
-	);
-	res.json(data);
+    redisClient.GET('photos',async (error,photos)=>{
+        if(error){
+            console.log(error);
+        }
+        if(photos!=null){
+            console.log("Cache Hit");
+            return res.json(JSON.parse(photos));
+        }else{
+            console.log("Cache Miss");
+            const { data } = await axios.get(
+                'http://jsonplaceholder.typicode.com/photos',
+                {
+                    params: { albumId },
+                }
+            );
+            redisClient.SETEX('photos', DEFAULT_EXPIRATION, JSON.stringify(data));
+            res.json(data);
+        }
+    })
 });
 
 app.get('/photos/:id', async (req, res) => {
